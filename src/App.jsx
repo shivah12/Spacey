7503d83df450e85fda294643711fa1ebd0078e69
Shreeti <114981873+shivah12@users.noreply.github.com>
2024-01-15 16:32:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-
-// Import Swiper components and modules
 import { EffectCoverflow, Pagination, Navigation } from 'swiper';
 
-// Import your images
 import slide_image_1 from './assets/images/img_1.png';
 import slide_image_2 from './assets/images/img_2.png';
 import slide_image_3 from './assets/images/img_3.png';
@@ -19,29 +14,22 @@ import slide_image_5 from './assets/images/img_5.png';
 import slide_image_6 from './assets/images/img_6.png';
 import slide_image_7 from './assets/images/img_7.png';
 import slide_image_8 from './assets/images/img_8.png';
-
-// Import the audio file
 import audioFile from './assets/audio.mp3';
 
 function App() {
   const audioRef = useRef(null);
+  const [audioMuted, setAudioMuted] = useState(true);
 
-  useEffect(() => {
-    // Start playing the audio when the component mounts
+  const handleInteraction = () => {
     if (audioRef.current) {
       audioRef.current.play();
+      setAudioMuted(false);
     }
-  }, []);
+  };
 
   return (
     <div className="container">
       <h1 className="heading">Spacey</h1>
-      <p className="subheading">A swiper with a space theme.</p>
-      <audio ref={audioRef} controls={false} loop>
-        <source src={audioFile} type="audio/mp3" />
-        Your browser does not support the audio tag.
-      </audio>
-
       <Swiper
         effect={'coverflow'}
         grabCursor={true}
@@ -62,6 +50,7 @@ function App() {
         }}
         modules={[EffectCoverflow, Pagination, Navigation]}
         className="swiper_container"
+        onSlideChange={handleInteraction}
       >
         <SwiperSlide>
           <img src={slide_image_1} alt="slide_image" />
@@ -95,7 +84,6 @@ function App() {
           <img src={slide_image_8} alt="slide_image" />
           <h2>Neptune</h2>
         </SwiperSlide>
-        
 
         <div className="slider-controler">
           <div className="swiper-button-prev slider-arrow">
@@ -107,6 +95,11 @@ function App() {
           <div className="swiper-pagination"></div>
         </div>
       </Swiper>
+
+      <audio ref={audioRef} controls={false} loop muted={audioMuted}>
+        <source src={audioFile} type="audio/mp3" />
+        Your browser does not support the audio tag.
+      </audio>
     </div>
   );
 }
